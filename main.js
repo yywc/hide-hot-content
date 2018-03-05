ecb13817555b0b484349b9d31e9f5f81b15ad1fc
guoyongjian <yongjian.guo@houkeyun.com>
2018-03-05 16:57:06 +0800

    first commit

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-var hideGarbage = function () {
-    var ele = document.getElementsByClassName('FeedSource-firstline');
-    for (var i = 0; i < ele.length; i++) {
-        var children = ele[i].children;
-        for (var j = 0; j < children.length; j++) {
-            var text = children[j].textContent;
-            if (text.match('热门内容')) {
-                children[j].parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+(function () {
+    var hideGarbage = function () {
+        var ele = document.getElementsByClassName('FeedSource-firstline');
+        for (var i = 0; i < ele.length; i++) {
+            var children = ele[i].children;
+            for (var j = 0; j < children.length; j++) {
+                var text = children[j].textContent;
+                if (text.match('热门内容')) {
+                    children[j].parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+                }
             }
         }
+    };
+
+    hideGarbage();
+
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+    // 选择目标节点
+    var target = document.getElementsByClassName('TopstoryMain')[0];
+    // 创建观察者对象
+    var observer = new MutationObserver(function (mutations) {
+        var newValue = target.dataset.zaExtraModule;
+        mutations.forEach(function (mutation) {
+            if (newValue !== mutation.oldValue) {
+                hideGarbage();
+            }
+        });
+    });
+    // 配置观察选项:
+    var config = {
+        attributes: true,
+        attributeOldValue: true
     }
-};
+    // 传入目标节点和观察选项
+    observer.observe(target, config);
 
-hideGarbage();
-
-window.addEventListener('scroll', hideGarbage);
+})();
